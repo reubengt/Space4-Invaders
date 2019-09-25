2dfd11695bdfbd699dc367250f126e6bbdfe5ebb
reubengt <reubengeorgethomas@gmail.com>
2019-09-25 16:28:13 +0100

    add request module to request for a specific github team's data. takes org and team as args

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import teamRequest from "./utils/request";
 function App() {
+  const Team = async () => {
+    const teamData = await teamRequest("FAC-17", "students");
+    const teamId = teamData.team_id;
+    console.log(teamData);
+    return <div>abcd</div>;
+  };
+  Team();
   return (
     <div className="App">
       <header className="App-header">
