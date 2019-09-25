a4f4b9e1ca9d5dae0e30505955e98ae5c7525546
Sam Harris <sam@jam-bc.com>
2019-09-25 16:56:24 +0100

    created views and rendering home page content

---
+++ src/views/homePage/homePage.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./homePage.css";
+
+const HomePage = () => {
+  return (
+    <>
+      <div>
+        <h1>Welcome to the best Name and Face learning game... EVER</h1>
+        <h4>
+          It is much better than Georgia and Anthony's terrible game whose faces
+          you will quickly forever associate with their names thanks to our
+          better game
+        </h4>
+      </div>
+    </>
+  );
+};
+
+export default HomePage;
