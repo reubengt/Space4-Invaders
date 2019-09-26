09580823ed4af4cfa79e7560d097c1f94070060d
Sam Harris <sam@jam-bc.com>
2019-09-26 12:11:05 +0100

    create game page and render from app depending on state

--- src/App.js
+++ src/App.js
@@ -3,16 +3,21 @@ import "./App.css";
 
 import requestMembers from "./utils/requestMembers";
 import HomePage from "./views/homePage/homePage";
+import GamePage from "./views/gamePage/gamePage";
+
 const App = () => {
+  const [page, setPage] = React.useState("home");
+
   const Team = async () => {
     const data = await requestMembers("FAC-17", "students");
     console.log("Data in component", data);
   };
+
   Team();
   return (
     <div className="App">
       <header className="App-header">Name the Face Game</header>
-      <HomePage />
+      {page === "home" ? <HomePage /> : <GamePage />}
     </div>
   );
 };
---
+++ src/views/gamePage/gamePage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./gamePage.css";
+
+const GamePage = () => {
+  return (
+    <>
+      <div>
+        <h1>GAME GAME GAME</h1>
+      </div>
+    </>
+  );
+};
+
+export default GamePage;
