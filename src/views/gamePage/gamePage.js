b931fdc19458c7d2582203442eb51abe5c1b8cdb
reubengt <reubengeorgethomas@gmail.com>
2019-09-26 17:15:53 +0100

    add state change depending on correct/incorrect selection

--- src/views/gamePage/gamePage.js
+++ src/views/gamePage/gamePage.js
@@ -1,9 +1,19 @@
 import React from "react";
 import "./gamePage.css";
-import { positionArray } from "./../../utils/constants";
+import {
+  positionArray,
+  sampleGameArray,
+  sampleCorrectArray
+} from "./../../utils/constants";
 const GamePage = () => {
   const [leftOffset, setleftOffset] = React.useState(positionArray[0]);
   const [selectedIndex, setselectedIndex] = React.useState(null);
+  const [currentPhase, setCurrentPhase] = React.useState(0);
+  const correctIndex = sampleCorrectArray[currentPhase];
+  console.log(correctIndex);
+  const correctName = sampleGameArray[currentPhase][correctIndex].name;
+  console.log(correctName);
+
   const styleObj = {
     left: leftOffset
   };
@@ -33,22 +43,47 @@ const GamePage = () => {
   }, []);
   return (
     <div className="main-container">
+      <h2>{correctName}</h2>
       <div className="game-container">
         <div className="column"></div>
         <div className="column"></div>
         <div className="column"></div>
         <div className="column"></div>
         <div
-          className={`img img1 ${selectedIndex === 0 ? "shot-down" : ""}`}
+          className={`img img1 ${
+            selectedIndex === 0 && selectedIndex === correctIndex
+              ? "shot-down-correct"
+              : selectedIndex === 0
+              ? "shot-down-incorrect"
+              : ""
+          }`}
         ></div>
         <div
-          className={`img img2 ${selectedIndex === 1 ? "shot-down" : ""}`}
+          className={`img img2 ${
+            selectedIndex === 1 && selectedIndex === correctIndex
+              ? "shot-down-correct"
+              : selectedIndex === 1
+              ? "shot-down-incorrect"
+              : ""
+          }`}
         ></div>
         <div
-          className={`img img3 ${selectedIndex === 2 ? "shot-down" : ""}`}
+          className={`img img3 ${
+            selectedIndex === 2 && selectedIndex === correctIndex
+              ? "shot-down-correct"
+              : selectedIndex === 2
+              ? "shot-down-incorrect"
+              : ""
+          }`}
         ></div>
         <div
-          className={`img img4 ${selectedIndex === 3 ? "shot-down" : ""}`}
+          className={`img img4 ${
+            selectedIndex === 3 && selectedIndex === correctIndex
+              ? "shot-down-correct"
+              : selectedIndex === 3
+              ? "shot-down-incorrect"
+              : ""
+          }`}
         ></div>
         <div className="player-character" style={styleObj}></div>
       </div>
